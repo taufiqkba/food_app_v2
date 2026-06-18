3553fa8dabf82cfad55767c3506e53a38fa44e94
taufiqkba <taufiqkurniawan9991@gmail.com>
2026-06-18 10:12:07 +0700

    feat: create table for migration auth.sql

--- docs/migration/auth.sql
+++ docs/migration/auth.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS auth (
-    id int 
-    public_id varchar
-    email varchar
-    password varchar
-    role varchar
-    is_active bool
-    created_at timestamp
-    updated_at timestamp
+    id SERIAL PRIMARY KEY, 
+    public_id TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL,
+    is_active boolean NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
 )
--- go.mod
+++ go.mod
@@ -4,8 +4,10 @@ go 1.26.4
 
 require (
 	github.com/go-chi/chi/v5 v5.3.0
+	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/lib/pq v1.12.3
 	github.com/stretchr/testify v1.11.1
+	golang.org/x/crypto v0.53.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
